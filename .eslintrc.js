c520ccae05a065fdd682df3596d83b0113332295
Dmitriy Zhiganov <delawere2009@yandex.ru>
2021-03-24 22:01:13 +0300

    fix a lot of lint problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,66 @@
 module.exports = {
-    root: true,
-    extends: [
-      'airbnb-typescript',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:jest/recommended',
-      'prettier',
-      'prettier/react',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-      'plugin:cypress/recommended',
-      'plugin:css-modules/recommended'
-    ],
-    plugins: ['react', '@typescript-eslint', 'jest', 'eslint-plugin-cypress', 'css-modules'],
-    env: {
-      browser: true,
-      es6: true,
-      jest: true,
-      'cypress/globals': true
+  root: true,
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
+    'plugin:css-modules/recommended',
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest',
+    'eslint-plugin-cypress',
+    'css-modules',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    'cypress/globals': true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      project: './tsconfig.json',
-    },
-    rules: {
-      'react/require-default-props': 'off',
-      'quotes': ['error', 'single'],
-      'linebreak-style': 'off',
-      'import/extensions': ['error', 'never'],
-      'no-param-reassign': ['error', {props: false}],
-      'import/prefer-default-export': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-          singleQuote: true,
-        },
-      ],
-    },
-
-    overrides: [
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    quotes: ['error', 'single'],
+    'linebreak-style': 'off',
+    'import/extensions': ['error', 'never'],
+    'no-param-reassign': ['error', { props: false }],
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': [
+      'error',
       {
-        files: ['cypress/**/*.js'],
-        rules: {
-          'jest/expect-expect': 'off'
-        }
-      }
-    ]
-  };
-  
+        endOfLine: 'auto',
+        singleQuote: true,
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ['cypress/**/*.js'],
+      rules: {
+        'jest/expect-expect': 'off',
+      },
+    },
+  ],
+};
