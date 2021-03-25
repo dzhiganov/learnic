a5381e0975e89fc969e6a6bbe83ef7432c3f2b82
Dmitriy Zhiganov <delawere2009@yandex.ru>
2021-03-25 21:33:46 +0300

    feature: add landing ru text

--- src/components/pages/Landing/index.tsx
+++ src/components/pages/Landing/index.tsx
@@ -17,13 +17,41 @@ const Landing: React.FC = () => {
   const handleClose = useCallback(() => setOpen(false), []);
 
   return (
-    <main>
+    <div>
       <header className={styles.header}>
         <div className={styles.top}>
+          <Logo size="large" />
           <Button title={t('LANDING.LOGIN')} onClick={handleClick} />
         </div>
-        <Logo size="large" />
       </header>
+      <main className={styles.main}>
+        <div className={`${styles.block} ${styles.mainBlock}`}>
+          <h3 className={styles.blockTitle}>{t('LANDING.MAIN.TITLE')}</h3>
+          <p>
+            <ul className={styles.list}>
+              <li>{t('LANDING.MAIN.LIST.0')}</li>
+              <li>{t('LANDING.MAIN.LIST.1')}</li>
+              <li>{t('LANDING.MAIN.LIST.2')}</li>
+              <li>{t('LANDING.MAIN.LIST.3')}</li>
+            </ul>
+          </p>
+        </div>
+        <div className={styles.block}>
+          <h3 className={styles.blockTitle}>
+            {t('LANDING.WHY_IT_WORKS.TITLE')}
+          </h3>
+          <p>
+            <ul className={styles.list}>
+              <li>{t('LANDING.WHY_IT_WORKS.LIST.0')}</li>
+              <li>{t('LANDING.WHY_IT_WORKS.LIST.1')}</li>
+            </ul>
+          </p>
+        </div>
+        <div className={styles.block}>
+          <h3 className={styles.blockTitle}>{t('LANDING.HOW_TO_USE.TITLE')}</h3>
+          <p>{t('LANDING.HOW_TO_USE.TEXT')}</p>
+        </div>
+      </main>
       <Modal
         open={open}
         onClose={handleClose}
@@ -32,7 +60,7 @@ const Landing: React.FC = () => {
       >
         <Login />
       </Modal>
-    </main>
+    </div>
   );
 };
 
