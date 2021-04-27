7067316476c6f168ad99aaeaf059cb8aa8ada2d1
Dmitriy Zhiganov <delawere2009@yandex.ru>
2021-04-27 22:01:36 +0300

    test

--- src/lambda/database.ts
+++ src/lambda/database.ts
@@ -1,11 +1,13 @@
 import 'firebase/firestore';
 import * as admin from 'firebase-admin';
 
+const privateKey = process.env.REACT_APP_PRIVATE_KEY as any;
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.REACT_APP_PROJECT_ID,
-      privateKey: process.env.REACT_APP_PRIVATE_KEY,
+      privateKey: privateKey[0] === '-' ? privateKey : JSON.parse(privateKey),
       clientEmail: process.env.REACT_APP_CLIENT_EMAIL,
     }),
     databaseURL: process.env.REACT_APP_DATABASE_URL,
