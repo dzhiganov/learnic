2e3356753329d4addcad6f5c4eea420262a70057
Dmitriy Zhiganov <delawere2009@yandex.ru>
2021-04-27 21:40:30 +0300

    fix lint errors

--- src/lambda/database.ts
+++ src/lambda/database.ts
@@ -1,16 +1,11 @@
 import 'firebase/firestore';
 import * as admin from 'firebase-admin';
 
-const a = process.env.REACT_APP_PROJECT_ID;
-const b = process.env.REACT_APP_PRIVATE_KEY;
-const c =  process.env.REACT_APP_CLIENT_EMAIL;
-
 if (!admin.apps.length) {
-  console.log('FOR TESTING ->', a, b, c);
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.REACT_APP_PROJECT_ID,
-      privateKey: process.env.REACT_APP_PRIVATE_KEY ? process.env.REACT_APP_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
+      privateKey: process.env.REACT_APP_PRIVATE_KEY,
       clientEmail: process.env.REACT_APP_CLIENT_EMAIL,
     }),
     databaseURL: process.env.REACT_APP_DATABASE_URL,
